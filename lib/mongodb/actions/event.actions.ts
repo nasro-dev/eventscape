bafa1d025c000a00e4f598732dcbf838247b5364
nasro-dev <boukhelkhalnasro@gmail.com>
2024-01-02 22:10:41 +0100

    event detail page

--- lib/mongodb/actions/event.actions.ts
+++ lib/mongodb/actions/event.actions.ts
@@ -5,8 +5,19 @@ import { CreateEventParams } from "@/types";
 import { connectToDb } from "../database";
 import User from "../models/user.model";
 import Event from "../models/event.model";
+import Category from "../models/category.model";
 
-
+const populateEvent = async(query: any) => {
+    return query.populate({
+        path:'organizer',
+        model:User,
+        select: '_id firstName lastName'
+    }).populate({
+        path: 'category',
+        model: Category,
+        select: '_id name'
+    })
+}
 
 export const createEvent = async ({ event, userId, path}:CreateEventParams) =>{
     try {
@@ -26,5 +37,20 @@ export const createEvent = async ({ event, userId, path}:CreateEventParams) =>{
 
     } catch (error) {
         handleError(error);   
+    }
+}
+
+export const getEventById = async (eventId: string) => {
+    try {
+        await connectToDb()
+        
+        const event = await populateEvent(Event.findById(eventId))
+        if(!event){
+            throw new Error("Event not found")
+        }
+
+        return JSON.parse(JSON.stringify(event))
+    } catch (error) {
+        handleError(error)
     }
 }
